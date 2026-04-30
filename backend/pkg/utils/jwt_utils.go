ec54044de942e41d199514da818b00ab643f8bd3
james-in-gz <ximingxie2012@gmail.com>
2026-04-30 09:34:48 +0800

    Add JWT utility functions for token generation and parsing

---
+++ backend/pkg/utils/jwt_utils.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte("your_secret_key")
+
+type Claims struct {
+	UserID uint
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(userID uint) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+
+	claims := &Claims{
+		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
+
+func ParseToken(tokenStr string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, err
+}
