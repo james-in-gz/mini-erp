68c23b157a7cd02ad513ea914fe1520197b51696
james-in-gz <ximingxie2012@gmail.com>
2026-05-02 00:04:36 +0800

    Add GetCustomerByPhone function to retrieve customer

--- backend/internal/repository/customer_repository.go
+++ backend/internal/repository/customer_repository.go
@@ -8,3 +8,14 @@ import (
 func CreateCustomer(customer *model.Customer) error {
 	return database.DB.Create(customer).Error
 }
+
+func GetCustomerByPhone(phone string) (*model.Customer, error) {
+	var customer model.Customer
+
+	err := database.DB.Where("phone = ?", phone).First(&customer).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &customer, nil
+}
