50c9fbedc363fdf80f51a39892066351023f0a81
james <westlife2011@qq.com>
2026-05-08 21:19:11 +0800

    fix: update database connection string to use correct timezone

--- backend/internal/database/db.go
+++ backend/internal/database/db.go
@@ -28,7 +28,7 @@ func InitDB() {
 
 	} else {
 		dsn := fmt.Sprintf(
-			"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+			"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Asia/ShangHai",
 			os.Getenv("DB_USER"),
 			os.Getenv("DB_PASSWORD"),
 			os.Getenv("DB_HOST"),
